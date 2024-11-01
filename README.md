88af3cbb9bcccfd4e711b2e199f6e9618da96a11
MoosaAwais14 <122404861+MoosaAwais14@users.noreply.github.com>
2024-11-01 00:18:47 -0400

    removed boilerplate and updated index

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <h1>Sources</h1>
-<br>
 <list>
 <li>W3Schools Top Navbar Tutorial: https://www.w3schools.com/howto/howto_js_topnav.asp</li>
 <li>W3Schools CSS Web Fonts: https://www.w3schools.com/css/css3_fonts.asp</li>
+<li>W3Schools Typewriter Effect Tutorial: https://www.w3schools.com/howto/howto_js_typewriter.asp
 <li>Digital Ocean Background Opacity Tutorial (appended for brightness): https://www.digitalocean.com/community/tutorials/how-to-change-a-css-background-images-opacity</li>
 </list>
--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
     $("#navbar").slideDown("slow");
 });
+
+
+// /*Modified Version of W3Schools's TypeWriter Tutorial*/
+// var i = 0;
+// var txt = document.getElementById("tw").textContent; /* The text */
+// var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+// function typeWriter() {
+//   if (i < txt.length) {
+//     document.getElementById('tw').innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
+
