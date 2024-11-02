45aa518e1c4c0af56bfbe4c58706722d33997f32
MoosaAwais14 <122404861+MoosaAwais14@users.noreply.github.com>
2024-11-01 23:54:09 -0400

    implemented typer.js and updated subpages

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 <h1>Sources</h1>
 <list>
-<ul>
 <li>W3Schools Top Navbar Tutorial: https://www.w3schools.com/howto/howto_js_topnav.asp</li>
 <li>W3Schools CSS Web Fonts: https://www.w3schools.com/css/css3_fonts.asp</li>
-<li>Digital Ocean Background Opacity Tutorial (appended for brightness): https://www.digitalocean.com/community/tutorials/how-to-change-a-css-background-images-opacity</li>
-<li>GreatStack Text Typing Animation Tutorial: https://www.youtube.com/watch?v=qEcXe51-ZTw
-<ul>
-  <li>
-    Used mattboldt's Typing Animation Library: https://github.com/mattboldt/typed.js/
-  </li>
-</ul>
-</ul>
+<li>Digital Ocean Background Opacity Tutorial (modified for brightness): https://www.digitalocean.com/community/tutorials/how-to-change-a-css-background-images-opacity</li>
+</list>
+
+<h1>Libraries/Additional Software</h1>
+<list>
+<li>Imported Google's <b>Orbitron</b> Font (Designed by <b>Matt McInerney</b>): https://fonts.google.com/specimen/Orbitron</li>
+<li>Imported <b>mattboldt's</b> Typing Animation Library (<b>Typed.js</b>): https://github.com/mattboldt/typed.js/
 </list>
--- script.js
+++ script.js
@@ -1,18 +1,62 @@
-$(document).ready(function () {
-    $("#navbar").slideDown("slow");
+$(document).ready(function () { // runs when document is loaded/ready
+    $("#navbar").slideDown("slow"); // applies slide down animation at slow speed to navbar 
 });
 
 
-// /*Modified Version of W3Schools's TypeWriter Tutorial*/
-// var i = 0;
-// var txt = document.getElementById("tw").textContent; /* The text */
-// var speed = 50; /* The speed/duration of the effect in milliseconds */
+function typewriter(text) {
+    /* 
+    Used mattboldt's repo/library, typed.js 
+    */
 
-// function typeWriter() {
-//   if (i < txt.length) {
-//     document.getElementById('tw').innerHTML += txt.charAt(i);
-//     i++;
-//     setTimeout(typeWriter, speed);
-//   }
-// }
+    // Defining constants
+    const typer = document.querySelector(".auto-type");
+    const fonts = ["Arial", "Courier New", "Orbitron"];
 
+    let i = 0; // will be used as a counter to alternate fonts
+
+    var typed = new Typed(".auto-type", {
+        strings: [text], // string being typed
+        typeSpeed: 60, // typer speed
+        backSpeed: 30, // speed of backspace
+        loop: true,
+        loopCount: 3, // will loop three times
+
+        preStringTyped: () => {  //runs before every type
+            if (i < 3)
+                typer.style.fontFamily = fonts[i]; // alternating font for the first two iterations
+            i++;
+        },
+
+        onComplete: () => { // runs everytime a type is completed
+            if (i >= 3) {
+                let t = document.querySelector(".auto-type");
+                t.style.color = "goldenrod"; // changing font color when final iteration is reached
+            }
+        },
+
+    });
+}
+
+const currentPage = window.location.pathname; // determing the current page
+
+// passing in different arguments to be typed out depending on current page
+if (currentPage.includes("index.html"))
+    typewriter("CyberLearn!");
+
+if (currentPage.includes("phish.html"))
+    typewriter("Phising?")
+
+if (currentPage.includes("threats.html"))
+    typewriter("Potential Threats?")
+
+if (currentPage.includes("triad.html"))
+    typewriter("CIA Triad?")
+
+if (currentPage.includes("pw.html"))
+    typewriter("Password?")
+
+if (currentPage.includes("authen.html"))
+    typewriter("Authentication?")
+
+if (currentPage.includes("3states.html"))
+    typewriter("3 States of Data?")
