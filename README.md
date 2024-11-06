299f5d9910e30cc6bc97ed74821e025a88360469
MoosaAwais14 <122404861+MoosaAwais14@users.noreply.github.com>
2024-11-05 21:08:43 -0500

    finished product

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-<h1>Sources</h1>
+<h1>Content Sources</h1>
+<li>Cyber.org</li>
+<li>SecurityScorecard: https://securityscorecard.com/blog/what-is-cybersecurity-risk-factors-to-consider/ </li>
+<h1>Development Sources</h1>
 <list>
 <li>W3Schools Top Navbar Tutorial: https://www.w3schools.com/howto/howto_js_topnav.asp</li>
 <li>W3Schools CSS Web Fonts: https://www.w3schools.com/css/css3_fonts.asp</li>
 <li>Digital Ocean Background Opacity Tutorial (modified for brightness): https://www.digitalocean.com/community/tutorials/how-to-change-a-css-background-images-opacity</li>
+<li>GeeksForGeek Sleep Function Tutorial: https://www.geeksforgeeks.org/how-to-add-sleep-wait-function-before-continuing-in-javascript</li>
 </list>
 
 <h1>Libraries/Additional Software</h1>
--- script.js
+++ script.js
@@ -3,60 +3,101 @@ $(document).ready(function () { // runs when document is loaded/ready
 });
 
 
-function typewriter(text) {
+$(document).ready(function () {
+    // Show the expanded content when the placeholder is clicked
+    $('#ph').on('click', function () {
+        $('#content').addClass('show');
+    });
+
+    // Hide the expanded content when the close button is clicked
+    $('#close').on('click', function () {
+        $('#content').removeClass('show');
+    });
+});
+
+
+
+function typewriter(text, className) {
     /* 
     Used mattboldt's repo/library, typed.js 
     */
 
     // Defining constants
-    const typer = document.querySelector(".auto-type");
-    const fonts = ["Arial", "Courier New", "Orbitron"];
+    const typer = document.querySelector(className);
+    const fonts = ["Arial", "Courier New", "Orbitron", "Fantasy"];
 
     let i = 0; // will be used as a counter to alternate fonts
 
-    var typed = new Typed(".auto-type", {
+    var typed = new Typed(className, {
         strings: [text], // string being typed
         typeSpeed: 60, // typer speed
         backSpeed: 30, // speed of backspace
         loop: true,
         loopCount: 3, // will loop three times
 
-        preStringTyped: () => {  //runs before every type
-            if (i < 3)
-                typer.style.fontFamily = fonts[i]; // alternating font for the first two iterations
-            i++;
+        preStringTyped: () => {  // runs before every type
+            if (i < 3) {
+                typer.style.color = "#fff";
+                typer.style.fontFamily = fonts[i]; // alternating font for the first two iterations       
+                i++;
+            }
         },
 
         onComplete: () => { // runs everytime a type is completed
-            if (i >= 3) {
-                let t = document.querySelector(".auto-type");
-                t.style.color = "goldenrod"; // changing font color when final iteration is reached
-            }
+            if (i == 3)
+                typer.style.color = "goldenrod"; // changing font color when final iteration is reached
         },
 
     });
 }
 
-const currentPage = window.location.pathname; // determing the current page
 
-// passing in different arguments to be typed out depending on current page
-if (currentPage.includes("index.html"))
-    typewriter("CyberLearn!");
+$(document).ready(function () {
+    // Show the expanded content when the placeholder is clicked
+    $('#ph').on('click', function () {
+        $('#content').addClass('show'); 
+    });
 
-if (currentPage.includes("phish.html"))
-    typewriter("Phising?");
+    // Hide the expanded content when the close button is clicked
+    $('#close').on('click', function () {
+        $('#content').removeClass('show');
+    });
+});
 
-if (currentPage.includes("threats.html"))
-    typewriter("Potential Threats?");
+const currentPage = window.location.pathname; // determining the current page
 
-if (currentPage.includes("triad.html"))
-    typewriter("CIA Triad?");
+// passing different arguments to be typed out depending on current page
+if (currentPage === "/CyberLearn/" || currentPage.includes("index.html"))
+    typewriter("CyberLearn!", ".auto-type");
 
-if (currentPage.includes("pw.html"))
-    typewriter("Password?");
+if (currentPage.includes("phish.html")){
+    typewriter("Phishing?", ".auto-type");
+    typewriter("Phishing?", ".auto");
+}
 
-if (currentPage.includes("authen.html"))
-    typewriter("Authentication?");
+if (currentPage.includes("threats.html")){
+    typewriter("Potential Threats?", ".auto-type");
+    typewriter("Potential Threats?", ".auto");
+}
 
-if (currentPage.includes("3states.html"))
-    typewriter("3 States of Data?");
+if (currentPage.includes("triad.html")){
+    typewriter("CIA Triad?", ".auto-type");
+    typewriter("CIA Triad?", ".auto");
+}
+
+if (currentPage.includes("pw.html")){
+    typewriter("Password?", ".auto-type");
+    typewriter("Password?", ".auto");
+}
+
+if (currentPage.includes("authen.html")){
+    typewriter("Authentication?", ".auto-type");
+    typewriter("Authentication?", ".auto");
+}
+
+if (currentPage.includes("3states.html")){
+    typewriter("3 States of Data?", ".auto-type");
+    typewriter("3 States of Data?", ".auto");
+}
+
+
